e958207e197f44017ab1f210507ea276cddee3c6
Alexandre Franca <Alexandre.Franca@williamhill.co.uk>
2022-10-06 19:35:26 +0100

    feat: Finishing off the Header,  extracting button from Header into HeadCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import mealsImage from '../../assets/meals.jpeg';
 import classes from "./Header.module.css";
+import HeadCartButton from './HeadCartButton';
 
 const Header = props => {
     return <React.Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <button>Cart</button>
+            <HeadCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt='table full of delicious foods'/>
