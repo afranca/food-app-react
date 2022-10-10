f95037cb0540790d5bb683d25bb41b87ee93fdc3
Alexandre Franca <Alexandre.Franca@williamhill.co.uk>
2022-10-10 08:30:21 +0100

    feat: Adding forgotten file

--- src/components/Layout/HeadCartButton.js
+++ src/components/Layout/HeadCartButton.js
@@ -1,17 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeadCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeadCartButton = (props) =>{
+    const [btnIsHilighted, setBtnIsHilighted] = useState(false);
     const cartCtx = useContext(CartContext);
-
+    
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${btnIsHilighted ? classes.bump : ''}`; 
+    const { items } = cartCtx;
+    useEffect(() =>{
+        if (items.length === 0){
+            return;
+        }
+        setBtnIsHilighted(true);
+
+        // Roll back the style after 300 milisec (time to perform bump effect)
+        const timer = setTimeout( ()=> {
+            setBtnIsHilighted(false);
+        }, 300);
+
+        // cleanup function
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
